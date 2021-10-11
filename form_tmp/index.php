889cf3211a61b06bd56f2e3d9711201be730a592
genImai <eichi_kite@yahoo.co.jp>
2021-10-11 20:44:46 +0900

    update002

--- form_tmp/index.php
+++ form_tmp/index.php
@@ -35,18 +35,18 @@
                 </div>
                 <div class="mb-3">
                     <label for="name" class="form-label">名前</label>
-                    <input placeholder="問合太郎" name="name" type="text" class="form-control" id="name" value="<?php if($_POST['name']){echo $_POST['name'];} ?>">
+                    <input placeholder="問合太郎" name="name" type="text" class="form-control" id="name" value="<?php if($_POST['name']){echo $_POST['name'];} ?>" required>
                 </div>
                 <div class="mb-3">
                     <label for="emaile" class="form-label">メールアドレス</label>
-                    <input placeholder="example@exlample.com" name="email" type="email" class="form-control" value="<?php if($_POST['email']){echo $_POST['email'];} ?>">
+                    <input placeholder="example@exlample.com" name="email" type="email" class="form-control" value="<?php if($_POST['email']){echo $_POST['email'];} ?>" required>
                 </div>
                 <div class="mb-3">
                     <label for="number" class="form-label">電話番号（ハイフンなし）</label>
                     <input placeholder="0000000000" name="number" type="text" class="form-control" id="number" value="<?php if($_POST['number']){echo $_POST['number'];} ?>">
                 <div class="mb-3">
                     <label for="msg" class="form-label">お問合せ内容</label>
-                    <textarea name="message" class="form-control" id="msg" rows="4"><?php if($_POST['message']){echo $_POST['message'];} ?></textarea>
+                    <textarea name="message" class="form-control" id="msg" rows="4" required><?php if($_POST['message']){echo $_POST['message'];} ?></textarea>
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto">
                     <button class="btn btn-primary" type="submit">確認</button>
