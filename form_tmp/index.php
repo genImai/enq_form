dec90d8424baabc9eb04ad3ee6b81c1e502f5ffb
genImai <eichi_kite@yahoo.co.jp>
2021-10-11 20:52:18 +0900

    update002

--- form_tmp/index.php
+++ form_tmp/index.php
@@ -25,7 +25,7 @@
         <div class="container-sm">
             <form action="confirm.php" method="POST">
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">件名</label>
+                    <label for="exampleInputEmail1" class="form-label">件名（必須）</label>
                     <select name="type" class="form-select" aria-label="Default select example" required>
                         <option value="" selected>未選択</option>
                         <option value="ご意見" <?php if($_POST['type']=='ご意見'){echo 'selected';} ?>>ご意見</option>
@@ -34,18 +34,18 @@
                     </select>
                 </div>
                 <div class="mb-3">
-                    <label for="name" class="form-label">名前</label>
+                    <label for="name" class="form-label">名前（必須）</label>
                     <input placeholder="問合太郎" name="name" type="text" class="form-control" id="name" value="<?php if($_POST['name']){echo $_POST['name'];} ?>" required>
                 </div>
                 <div class="mb-3">
-                    <label for="emaile" class="form-label">メールアドレス</label>
+                    <label for="emaile" class="form-label">メールアドレス（必須）</label>
                     <input placeholder="example@exlample.com" name="email" type="email" class="form-control" value="<?php if($_POST['email']){echo $_POST['email'];} ?>" required>
                 </div>
                 <div class="mb-3">
                     <label for="number" class="form-label">電話番号（ハイフンなし）</label>
                     <input placeholder="0000000000" name="number" type="text" class="form-control" id="number" value="<?php if($_POST['number']){echo $_POST['number'];} ?>">
                 <div class="mb-3">
-                    <label for="msg" class="form-label">お問合せ内容</label>
+                    <label for="msg" class="form-label">お問合せ内容（必須）</label>
                     <textarea name="message" class="form-control" id="msg" rows="4" required><?php if($_POST['message']){echo $_POST['message'];} ?></textarea>
                 </div>
                 <div class="d-grid gap-2 col-6 mx-auto">
