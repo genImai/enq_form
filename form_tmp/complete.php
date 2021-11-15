16cefe1a000a02fb14e877c5631f8c9b1fde91ff
genImai <eichi_kite@yahoo.co.jp>
2021-11-15 22:46:44 +0900

    update20211115

--- form_tmp/complete.php
+++ form_tmp/complete.php
@@ -25,47 +25,47 @@
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
             -->
         </div>
+        <?php
+        require_once('db.php');
+        require_once('mail.php');
+
+        $enq_type = $_POST['type'];
+        $enq_name = $_POST['name'];
+        $enq_email = $_POST['email'];
+        $enq_num = $_POST['number'];
+        $enq_message = $_POST['message'];
+
+        $enq_type = htmlspecialchars($enq_type,ENT_QUOTES,'UTF-8');
+        $enq_name = htmlspecialchars($enq_name,ENT_QUOTES,'UTF-8');
+        $enq_email = htmlspecialchars($enq_email,ENT_QUOTES,'UTF-8');
+        $enq_num = htmlspecialchars($enq_num,ENT_QUOTES,'UTF-8');
+        $enq_message = htmlspecialchars($enq_message,ENT_QUOTES,'UTF-8');
+
+        try {
+            //DB接続とクエリ
+            $dsn = "mysql:host={$host};dbname={$dbName};charser=utf8";
+            $dbh = new PDO($dsn, $user, $password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO enq_list(type,name,email,number,message) VALUES(?,?,?,?,?)";
+            $stmt = $dbh->prepare($sql);
+            $data[] = $enq_type;
+            $data[] = $enq_name;
+            $data[] = $enq_email;
+            $data[] = $enq_num;
+            $data[] = $enq_message;
+
+            $stmt->execute($data);
+
+            $dbh = null;
+
+            //メール送信
+            mb_send_mail($to1, $subject1, $body1, $header1);
+            mb_send_mail($to2, $subject2, $body2, $header2);
+        } 
+        catch(PDOException $e) {
+            print 'データベースエラーが発生しました';
+            exit();
+        }
+        ?>
     </body>
 </html>
-<?php
-require_once('db.php');
-require_once('mail.php');
-
-$type = $_POST['type'];
-$name = $_POST['name'];
-$mailTo = $_POST['email'];
-$number = $_POST['number'];
-$message = $_POST['message'];
-$log = "";
-
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    $log = " DB接続成功\n";
-    echo '<script>';
-    echo 'console.log('. json_encode( $log ) .')';
-    echo '</script>';
-} catch(PDOException $e) {
-    $log = "DB接続失敗: " . $e->getMessage() . "\n";
-    echo '<script>';
-    echo 'console.log('. json_encode( $log ) .')';
-    echo '</script>';
-    exit();
-}
-$sql = "INSERT INTO enq_list(type, name, email, number, message) VALUES(:type, :name, :email, :number, :message)";
-$stmt = $pdo -> prepare($sql);
-$stmt->bindValue(':type', $type);
-$stmt->bindValue(':name', $name);
-$stmt->bindValue(':email', $mailTo);
-$stmt->bindValue(':number', $number);
-$stmt->bindValue(':message', $message);
-
-$stmt->execute();
-
-// DB接続を閉じる
-$pdo = null;
-//メール送信
-mb_send_mail($to1, $subject1, $body1, $header1);
-mb_send_mail($to2, $subject2, $body2, $header2);
-
-
-?>
--- form_tmp/confirm.php
+++ form_tmp/confirm.php
@@ -11,7 +11,7 @@
     <title>お問合せフォーム（確認）</title>
     </head>
     <body>
-    <h1>お問合せフォーム（確認）</h1>
+        <h1>お問合せフォーム（確認）</h1>
     <!-- Optional JavaScript; choose one of the two! -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- Option 1: Bootstrap Bundle with Popper -->
@@ -22,41 +22,54 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     -->
+    <?php
+    $enq_type = $_POST['type'];
+    $enq_name = $_POST['name'];
+    $enq_email = $_POST['email'];
+    $enq_num = $_POST['number'];
+    $enq_message = $_POST['message'];
+
+    $enq_type = htmlspecialchars($enq_type,ENT_QUOTES,'UTF-8');
+    $enq_name = htmlspecialchars($enq_name,ENT_QUOTES,'UTF-8');
+    $enq_email = htmlspecialchars($enq_email,ENT_QUOTES,'UTF-8');
+    $enq_num = htmlspecialchars($enq_num,ENT_QUOTES,'UTF-8');
+    $enq_message = htmlspecialchars($enq_message,ENT_QUOTES,'UTF-8');
+    ?>
     <div class="container-sm">
         <form action="" method="POST">
             <table class="table">
                 <tbody>
                     <tr>
                         <th>件名</th>
-                        <td><?php echo $_POST['type'] ?></td>
-                        <input type="hidden" name="type" value="<?php echo $_POST['type'] ?>">
+                        <td><?php echo $enq_type ?></td>
+                        <input type="hidden" name="type" value="<?php echo $enq_type ?>">
                     </tr>
                     <tr>
                         <th>名前</th>
-                        <td><?php echo $_POST['name'] ?></td>
-                        <input type="hidden" name="name" value="<?php echo $_POST['name'] ?>">
+                        <td><?php echo $enq_name ?></td>
+                        <input type="hidden" name="name" value="<?php echo $enq_name ?>">
                     </tr>
                     <tr>
                         <th>メールアドレス</th>
-                        <td><?php echo $_POST['email'] ?></td>
-                        <input type="hidden" name="email" value="<?php echo $_POST['email'] ?>">
+                        <td><?php echo $enq_email ?></td>
+                        <input type="hidden" name="email" value="<?php echo $enq_email ?>">
                     </tr>
                     <tr>
                         <th>電話番号</th>
                         <td>
                             <?php 
-                                $numChecked = mb_convert_kana($_POST['number'], 'n', 'UTF-8'); 
+                                $numChecked = mb_convert_kana($enq_num, 'n', 'UTF-8'); 
                                 $numChecked = str_replace('-','',$numChecked);
-                                $_POST['number'] = $numChecked;
-                                echo $_POST['number'];
+                                $enq_num = $numChecked;
+                                echo $enq_num;
                             ?>
                         </td>
-                        <input type="hidden" name="number" value="<?php echo $_POST['number'] ?>">
+                        <input type="hidden" name="number" value="<?php echo $enq_num ?>">
                     </tr>
                     <tr>
                         <th>お問合せ内容</th>
-                        <td><?php echo $_POST['message'] ?></td>
-                        <input type="hidden" name="message" value="<?php echo $_POST['message'] ?>">
+                        <td><?php echo $enq_message ?></td>
+                        <input type="hidden" name="message" value="<?php echo $enq_message ?>">
                     </tr>
                 </tbody>
             </table>
